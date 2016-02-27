3e68a48e778e476ea4852b25c05bea6de4b2cd46
fonluiz <luiz.fonseca@ccc.ufcg.edu.br>
2016-02-27 17:14:13 -0300

    criado o list fragment do feed inicial

--- app/src/main/java/com/example/projetoes/projetoes/Activities/LostFound.java
+++ app/src/main/java/com/example/projetoes/projetoes/Activities/LostFound.java
@@ -1,4 +1,4 @@
-package com.example.projetoes.projetoes;
+package com.example.projetoes.projetoes.Activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.projetoes.projetoes.R;
 
 public class LostFound extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
