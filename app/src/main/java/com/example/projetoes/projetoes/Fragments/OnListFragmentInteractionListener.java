d3bfaf1dd843d0856bf12060b13f795c988241f9
Samir Silva de Medeiros <s.silvademedeiros@mail.utoronto.ca>
2016-02-28 01:14:56 -0300

    Ajustando uns bugs iniciais

---
+++ app/src/main/java/com/example/projetoes/projetoes/Fragments/OnListFragmentInteractionListener.java
@@ -0,0 +1,17 @@
+package com.example.projetoes.projetoes.Fragments;
+
+import com.example.projetoes.projetoes.dummy.DummyContent;
+
+/**
+ * Created by Samir Silva e Luiz Alberto on 27/02/16.
+ * Interface que gerencia as açoes de clique nos itens do feed principal.
+ *
+ */
+public interface OnListFragmentInteractionListener {
+
+    /**
+     * Lida com a iteração de cliques nos itens do List Fragment
+     * @param item O item que recebeu uma ação, um toque simples por exemplo.
+     */
+    public void onListFragmentInteraction(DummyContent.DummyItem item);
+}
