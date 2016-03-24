89310e1a58ffdd96402cdcaab75a2998c8108490
fonluiz <luiz.fonseca@ccc.ufcg.edu.br>
2016-03-24 08:40:34 -0300

    Criar tela de editar perfil

---
+++ app/src/main/java/com/example/projetoes/projetoes/Fragments/EditProfileFragment.java
@@ -0,0 +1,44 @@
+package com.example.projetoes.projetoes.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.projetoes.projetoes.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EditProfileFragment extends Fragment {
+
+    public final static String TAG = "EDIT_PROFILE_FRAGMENT";
+    private View mView;
+
+    public EditProfileFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static ProfileFragment newInstance() {
+        ProfileFragment fragment = new ProfileFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mView = inflater.inflate(R.layout.fragment_edit_profile, container, false);
+
+        return mView;
+    }
+
+}
